09c00576c867f62146e2493738d85720b045e698
Diegodenac <diegocanedo917@gmail.com>
2024-03-19 03:11:09 -0400

    Prueba de Score para 20 lanzamientos de 0 puntos g.score()=>0

--- src/bowlingGame.test.js
+++ src/bowlingGame.test.js
@@ -1,8 +1,19 @@
 describe("BowlingGameTest", () => {
   it("Deberia obtener el Score total", () => {
     const g = new Game();
+    for(let i=0; i<20; i++){
+      g.roll(0);
+    }
+    expect(g.score()).toEqual(0);
   });
 });
 
 class Game {
+  roll(pins){
+
+  }
+  score(){
+    return 0;
+  }
+
 }
