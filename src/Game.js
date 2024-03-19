46cb853130e438627d8ab5f9833fc4c5958b01e6
Diegodenac <diegocanedo917@gmail.com>
2024-03-19 04:50:26 -0400

    Refactoring funcion booleana isStrike para funcion score

--- src/Game.js
+++ src/Game.js
@@ -13,6 +13,10 @@ class Game {
     return this.rolls[frameIndex]+this.rolls[frameIndex+1] == 10;
   }
 
+  isStrike(frameIndex){
+    return this.rolls[frameIndex] == 10;
+  }
+
   sumOfBallsInframe(frameIndex){
     return this.rolls[frameIndex]+this.rolls[frameIndex+1];
   }
@@ -27,7 +31,7 @@ class Game {
     let score = 0;
     let frameIndex=0;
     for(let frame=0; frame<10; frame++){
-      if(this.rolls[frameIndex] == 10) //strike
+      if(this.isStrike(frameIndex)) //strike
       {
         score+=this.strikeBonus(frameIndex);
         frameIndex++;
