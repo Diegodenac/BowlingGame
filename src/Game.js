66547b3b2b7bac60b3ca55adcd8d9ce770c90e6c
Diegodenac <diegocanedo917@gmail.com>
2024-03-19 04:26:47 -0400

    Refactoring score y funcion booleana isIndex added

--- src/Game.js
+++ src/Game.js
@@ -9,18 +9,22 @@ class Game {
     this.currentRoll++;
   }
 
+  isSpare(frameIndex){
+    return this.rolls[frameIndex]+this.rolls[frameIndex+1] == 10;
+  }
+
   score() {
     let score = 0;
-    let i=0;
+    let frameIndex=0;
     for(let frame=0; frame<10; frame++){
-      if(this.rolls[i]+this.rolls[i+1] == 10) //spare
+      if(this.isSpare(frameIndex)) //spare
       {
-        score += 10+this.rolls[i+2];
-        i+=2;
+        score += 10+this.rolls[frameIndex+2];
+        frameIndex+=2;
       }
       else{
-        score +=this.rolls[i]+this.rolls[i+1];
-        i+=2;
+        score +=this.rolls[frameIndex]+this.rolls[frameIndex+1];
+        frameIndex+=2;
       }
     }
     return score; 
