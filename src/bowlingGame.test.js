d8b48aca4e3315cd868d1a02bb9a604514736616
Diegodenac <diegocanedo917@gmail.com>
2024-03-19 03:32:18 -0400

    Implementando variables para generalizar pruebas g.score() dado n = 20 y pins = 0 =>0

--- src/bowlingGame.test.js
+++ src/bowlingGame.test.js
@@ -3,8 +3,10 @@ import Game from "./Game";
 describe("BowlingGameTest para obtener Score", () => {
   it("Deberia obtener el Score total para 20 tiros de 0 puntos", () => {
     const g = new Game();
-    for(let i=0; i<20; i++){
-      g.roll(0);
+    let n = 20;
+    let pins = 0; 
+    for(let i=0; i<n; i++){
+      g.roll(pins);
     }
     expect(g.score()).toEqual(0);
   });
@@ -16,4 +18,6 @@ describe("BowlingGameTest para obtener Score", () => {
     }
     expect(g.score()).toEqual(20);
   });
+
+  
 });
